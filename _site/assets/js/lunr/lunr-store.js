353a94c6f0fdff50aaae334a2d9f4ef115922a71
Collin <collin.chew@usmc.mil>
2020-09-17 16:26:26 -0400

    formatting

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Hello World",
-        "excerpt":"Hello world!  ","categories": ["announcements","news"],
-        "tags": ["coding","firstpost"],
-        "url": "http://localhost:4000/announcements/news/Hello-World/",
-        "teaser": null
-      }]
